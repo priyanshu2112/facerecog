973c07d510d33cceac0d1c102ec601d1ed470819
Priyanshu <priyanshu2112@gmail.com>
2018-02-22 11:59:39 +0530

    commit

---
+++ config.js
@@ -0,0 +1,13 @@
+/*!
+ * config
+ */
+
+module.exports = {
+    port: 3000,
+    client_id: '812105112314036',
+    client_secret: 'c78a52605a942969611a6c9fb882cb0d',
+    scope: 'publish_actions',
+    cookies: 'datr=4h1oWgvIVLI8leh-YAt58O5m; sb=9B1oWvEuonzHHfxFVBv83tHS; dpr=1.25; c_user=100000826999273; xs=3%3AhcAGV76B7Adl0A%3A2%3A1516772884%3A19785%3A5076; pl=n; fr=0nUNj3sKjhbwqEo4O.AWVSsiMnSkY7aPFYZFRI9dJlAXo.BaXYzJ.BP.FqF.0.0.Bai7b1.; presence=EDvF3EtimeF1519111311EuserFA21B00826999273A2EstateFDutF1519111311236CEchFDp_5f1B00826999273F14CC; wd=944x759; act=1519111367154%2F6', // put all of your cookies here
+    req_params: '__user=100000826999273&__a=1&__dyn=5V4cjLx2ByK5A9UkKHqAyqomzFEvzEyGgS8UR9LOaErx-6edwFF6xCahUKFGUpxSaxu9wwz8KFUKbnyogyEnGi4FpeuUG4WgWbxai6FQ5o8oHzo9VrCAUg-uu-egmVV8Gicx2q5od8nx29AAy8G5p8hwCwBxrxqrVe9CJ4gqx-EmDzFK4byUG68y4UlDBgS7E8VEGqq8xmeDx6WLBx11yhudKmloyHykh6cGxm5oJ2poCVk5kUKVEky4eCjCzobFEpDyebyaG4uEgy9Ehx6aCx-cGbQnyVeimWDU8fiAxqtAoOpy8qAh8G4ECmUhxu4Kl2qK&__req=2p&__be=1&__pc=PHASED%3ADEFAULT&__rev=3659592&fb_dtsg=AQFR2WiKqV7n%3AAQHF1Helm1NJ&jazoest=265817082508710575113865511058658172704972101108109497874&__spin_r=3659592&__spin_b=trunk&__spin_t=1519111068' // read README.md
+
+};
